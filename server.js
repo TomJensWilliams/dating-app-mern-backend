714ec727f4902a380232512da5fc7514aa70ed46
Thomas Williams <tomjenswilliams@gmail.com>
2024-07-22 14:10:23 -0700

    Finished with instructions version.

---
+++ server.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Cors from 'cors';
+import dotenv from 'dotenv';
+import Cards from './dbCards.js';
+
+dotenv.config();
+
+// App config
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url = `mongodb+srv://tomjenswilliams:${process.env.MONGO_PASSWORD}@cluster0.hpqxgdo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(process.env.MONGO_PASSWORD);
+console.log(
+  `mongodb+srv://tomjenswilliams:${process.env.MONGO_PASSWORD}@cluster0.hpqxgdo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+);
+
+// Middleware
+app.use(express.json());
+app.use(Cors());
+
+// DB Config
+mongoose.connect(connection_url, {
+  /*
+  useNewUrlParser: true,
+  useCreatedIndex: true,
+  useUnifiedTypology: true,
+  */
+});
+
+// API Endpoints
+app.get('/', (req, res) => res.status(200).send('Hello TheWebDev'));
+
+app.post('/dating/cards', async (req, res) => {
+  try {
+    const dbCard = req.body;
+    const data = await Cards.create(dbCard);
+    res.status(201).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.get('/dating/cards', async (req, res) => {
+  try {
+    let data = await Cards.find();
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+// Listener
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
